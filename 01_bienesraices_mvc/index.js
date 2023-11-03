fb0feac6d3a3b5a8c3e23e3d8c9ea0aebd3bd196
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-11-03 18:31:30 -0300

    01_bienesraices_mvc-creando un servidor con express

--- 01_bienesraices_mvc/index.js
+++ 01_bienesraices_mvc/index.js
@@ -1,1 +1,19 @@
-console.log('Hola Mundo!!!');
+const express = require('express');
+
+// Crear la app
+const app = express();
+
+// Routing
+app.get('/', function(req, res) {
+    res.send('Hola Mundo en express!!!');
+});
+
+app.get('/nosotros', function(req, res) {
+    res.json({msg: 'Información de Nosotros'});
+});
+
+// Definir un puerto y arrancar el proyecto
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`El Servidor esta funcionando en el puerto ${port}`);
+});
