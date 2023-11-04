aae2b586be92215e62a81c79f2e2c3421faeeecf
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-11-04 04:16:33 -0300

    01_bienesraices_mvc-añadiendo más rutas

--- 01_bienesraices_mvc/controllers/usuarioController.js
+++ 01_bienesraices_mvc/controllers/usuarioController.js
@@ -1,9 +1,16 @@
 const formularioLogin = (req, res) => {
     res.render('auth/login', {
-        autenticado: false
+        
+    });
+}
+
+const formularioRegistro = (req, res) => {
+    res.render('auth/registro', {
+        
     });
 }
 
 export { 
-    formularioLogin
+    formularioLogin,
+    formularioRegistro
 }
