cfdbcb29877dcd0ce2524debad5d6537ee597af3
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-11-04 03:47:11 -0300

    01_bienesraices_mvc-probando peticiones get y post

--- 01_bienesraices_mvc/routes/usuarioRoutes.js
+++ 01_bienesraices_mvc/routes/usuarioRoutes.js
@@ -2,12 +2,12 @@ import express from "express";
 
 const router = express.Router();
 
-router.get('/', function(req, res) {
-    res.send('Hola Mundo en express!!!');
+router.get('/', (req, res) => {
+    res.send({msg: 'Hola Mundo en express!!!'});
 });
 
-router.get('/nosotros', function(req, res) {
-    res.json({msg: 'Información de Nosotros'});
+router.post('/', (req, res) => {
+    res.json({msg: 'Respuesta de tipo post'});
 });
 
 export default router;
