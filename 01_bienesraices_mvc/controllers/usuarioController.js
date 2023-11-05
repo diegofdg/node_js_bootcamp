e2c491f1cc44d5690d0617266befee82e73edf3b
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-11-05 08:05:24 -0300

    01_bienesraices_mvc-creando la página de olvide mi password

--- 01_bienesraices_mvc/controllers/usuarioController.js
+++ 01_bienesraices_mvc/controllers/usuarioController.js
@@ -12,7 +12,15 @@ const formularioRegistro = (req, res) => {
     });
 }
 
+const formularioOlvidePassword = (req, res) => {
+    res.render('auth/olvide-password', {
+        pagina: 'Recupera tu acceso a Bienes Raices'
+        
+    });
+}
+
 export { 
     formularioLogin,
-    formularioRegistro
+    formularioRegistro,
+    formularioOlvidePassword
 }
