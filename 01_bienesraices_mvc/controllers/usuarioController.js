85e3c15fd7973429f4b2deeccc2de9beacbccba8
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-11-05 15:50:58 -0300

    01_bienesraices_mvc-autollenando los campos que pasaron la validación

--- 01_bienesraices_mvc/controllers/usuarioController.js
+++ 01_bienesraices_mvc/controllers/usuarioController.js
@@ -16,11 +16,12 @@ const formularioRegistro = (req, res) => {
 }
 
 const registrar = async (req, res) => {
+
     // Validación
     await check('nombre').notEmpty().withMessage('El Nombre no puede ir vacio').run(req);
     await check('email').isEmail().withMessage('Eso no parece un email').run(req);
     await check('password').isLength({ min: 6 }).withMessage('El Password debe ser de al menos 6 caracteres').run(req);
-    await check('repetir_password').equals('password').withMessage('Los Passwords no son iguales').run(req);
+    await check('repetir_password').equals(req.body.password).withMessage('Los Passwords no son iguales').run(req);
 
     let resultado = validationResult(req);
 
@@ -30,10 +31,12 @@ const registrar = async (req, res) => {
         return res.render('auth/registro', {
             pagina: 'Crear Cuenta',
             errores: resultado.array(),
+            usuario: {
+                nombre: req.body.nombre,
+                email: req.body.email
+            }
         })
     }
-
-    res.json(resultado.array());
 
     const usuario = await Usuario.create(req.body);
     res.json(usuario);
