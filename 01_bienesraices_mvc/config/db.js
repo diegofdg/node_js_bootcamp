471b9a0557e259c5b4b264d349326a5e14db47ae
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-11-05 15:11:15 -0300

    01_bienesraices_mvc-creando un usuario desde el formulario

--- 01_bienesraices_mvc/config/db.js
+++ 01_bienesraices_mvc/config/db.js
@@ -15,7 +15,8 @@ const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env
         acquire: 30000,
         idle: 10000
     }, 
-    operatorAliases: false
+    operatorAliases: false,
+    logging: false
 });
 
 export default db;
--- 01_bienesraices_mvc/controllers/usuarioController.js
+++ 01_bienesraices_mvc/controllers/usuarioController.js
@@ -1,3 +1,5 @@
+import Usuario from '../models/Usuario.js';
+
 const formularioLogin = (req, res) => {
     res.render('auth/login', {
         pagina: 'Iniciar Sesión'
@@ -12,8 +14,10 @@ const formularioRegistro = (req, res) => {
     });
 }
 
-const registrar = (req, res) => {
-    console.log(req.body);
+const registrar = async (req, res) => {
+    const usuario = await Usuario.create(req.body);
+    res.json(usuario);
+
 }
 
 const formularioOlvidePassword = (req, res) => {
