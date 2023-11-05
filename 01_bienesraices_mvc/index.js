636b9823579347d770ba44b86ee2c555f8e0d676
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-11-05 08:36:15 -0300

    01_bienesraices_mvc-conectando nuestra app a una base de datos

--- 01_bienesraices_mvc/index.js
+++ 01_bienesraices_mvc/index.js
@@ -1,8 +1,18 @@
 import express from 'express';
-import usuarioRoutes from './routes/usuarioRoutes.js';  
+import usuarioRoutes from './routes/usuarioRoutes.js';
+import db from './config/db.js';
 
 // Crear la app
 const app = express();
+
+// Conexión a la base de datos
+try {
+    await db.authenticate();
+    db.sync();
+    console.log('Conexión Correcta a la Base de datos');
+} catch (error) {
+    console.log(error);
+}
 
 // Habilitar Pug
 app.set('view engine', 'pug');
