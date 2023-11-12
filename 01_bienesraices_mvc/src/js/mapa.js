5b9dd37fbfb230b1208dfe0836125352b5a108cb
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-11-12 11:52:15 -0300

    01_bienesraices_mvc-mostrar el mapa

--- 01_bienesraices_mvc/src/js/mapa.js
+++ 01_bienesraices_mvc/src/js/mapa.js
@@ -1,1 +1,12 @@
-alert('Hola mapa!!!');
+(function() {
+
+    // Logical Or
+    const lat = 34.040967;
+    const lng = -118.1618621;
+    const mapa = L.map('mapa').setView([lat, lng ], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mapa);
+
+})()
