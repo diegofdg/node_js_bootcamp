31bef9f2afb866c032c00f26676899b443051be2
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-11-12 12:20:58 -0300

    01_bienesraices_mvc-almacenando latitud, longitud y calle

--- 01_bienesraices_mvc/src/js/mapa.js
+++ 01_bienesraices_mvc/src/js/mapa.js
@@ -30,6 +30,12 @@
             // console.log(resultado);
 
             marker.bindPopup(resultado.address.LongLabel);            
+
+            // Llenar los campos
+            document.querySelector('.calle').textContent = resultado?.address?.Address ?? '';
+            document.querySelector('#calle').value = resultado?.address?.Address ?? '';
+            document.querySelector('#lat').value = resultado?.latlng?.lat ?? '';
+            document.querySelector('#lng').value = resultado?.latlng?.lng ?? '';
         });
     });
 
