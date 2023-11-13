3f61fd3074f9c3beeb68f6d0ffcc93671197e206
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-11-13 07:33:17 -0300

    01_bienesraices_mvc-ejecutar el seeder

--- 01_bienesraices_mvc/seed/seeder.js
+++ 01_bienesraices_mvc/seed/seeder.js
@@ -1,7 +1,7 @@
 import { exit } from 'node:process';
 import categorias from './categorias.js';
 import db from '../config/db.js';
-import { Categoria } from '../models/index.js';
+import Categoria from '../models/Categoria.js';
 
 const importarDatos = async () => {
     try {
@@ -21,4 +21,8 @@ const importarDatos = async () => {
         console.log(error);
         exit(1);
     }
+}
+
+if(process.argv[2] === "-i") {
+    importarDatos();
 }
