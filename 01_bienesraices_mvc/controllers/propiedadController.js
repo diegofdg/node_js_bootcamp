3d25465ac5333421112d673f9862f600faa3390b
diegofdg <diegofdg@gmail.com>
2023-11-20 19:57:17 -0300

    01_bienesraices_mvc-agregando el usuario autenticado a la propiedad recién creada

--- 01_bienesraices_mvc/controllers/propiedadController.js
+++ 01_bienesraices_mvc/controllers/propiedadController.js
@@ -53,6 +53,8 @@ const guardar = async (req, res) => {
     // Crear un Registro
 
     const { titulo, descripcion, habitaciones, estacionamiento, wc, calle, lat, lng, precio: precioId, categoria: categoriaId } = req.body;
+    
+    const { id: usuarioId } = req.usuario;
 
     try {
         const propiedadGuardada = await Propiedad.create({
@@ -65,8 +67,14 @@ const guardar = async (req, res) => {
             lat,
             lng,
             precioId,
-            categoriaId
+            categoriaId,
+            usuarioId,
+            imagen: ''
         });
+
+        const { id } = propiedadGuardada;
+
+        res.redirect(`/propiedades/agregar-imagen/${id}`);
 
     } catch (error) {
         console.log(error);
