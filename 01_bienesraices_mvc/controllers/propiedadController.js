f56450ef6512c59f4ce1a5edf219a967694f9653
diegofdg <diegofdg@gmail.com>
2023-11-20 17:18:37 -0300

    01_bienesraices_mvc-leer los datos del formulario para crear la propiedad

--- 01_bienesraices_mvc/controllers/propiedadController.js
+++ 01_bienesraices_mvc/controllers/propiedadController.js
@@ -1,6 +1,5 @@
 import { validationResult } from 'express-validator';
-import Precio from '../models/Precio.js';
-import Categoria from '../models/Categoria.js';
+import { Precio, Categoria, Propiedad } from '../models/index.js';
 
 
 const admin = async (req, res) => {
@@ -49,7 +48,29 @@ const guardar = async (req, res) => {
             errores: resultado.array(),
             datos: req.body
         });
-    }    
+    }
+    
+    // Crear un Registro
+
+    const { titulo, descripcion, habitaciones, estacionamiento, wc, calle, lat, lng, precio: precioId, categoria: categoriaId } = req.body;
+
+    try {
+        const propiedadGuardada = await Propiedad.create({
+            titulo,
+            descripcion,
+            habitaciones, 
+            estacionamiento, 
+            wc,
+            calle,
+            lat,
+            lng,
+            precioId,
+            categoriaId
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export {
