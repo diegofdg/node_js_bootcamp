92fdacd52027dd042baa301e5510695aad4f41dc
diegofdg <diegofdg@gmail.com>
2023-11-20 19:26:18 -0300

    01_bienesraices_mvc-creando un custom middleware para validar rutas

---
+++ 01_bienesraices_mvc/middleware/protegerRuta.js
@@ -0,0 +1,10 @@
+const protegerRuta = async (req, res, next) => {
+
+    // Verificar si hay un token
+    
+    // Comprobar el Token
+    
+    next();    
+}
+
+export default protegerRuta;
--- 01_bienesraices_mvc/routes/propiedadesRoutes.js
+++ 01_bienesraices_mvc/routes/propiedadesRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { body } from 'express-validator';
 import { admin, crear, guardar } from '../controllers/propiedadController.js';
+import protegerRuta from "../middleware/protegerRuta.js";
 
 const router = express.Router();
 
-router.get('/propiedades', admin);
+router.get('/propiedades', protegerRuta, admin);
 router.get('/propiedades/crear', crear);
 router.post('/propiedades/crear',
     body('titulo').notEmpty().withMessage('El Titulo del Anuncio es Obligatorio'),
