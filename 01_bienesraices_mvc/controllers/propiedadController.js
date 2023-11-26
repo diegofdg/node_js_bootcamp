debab294284659770f8d4b1afb1c20bd6c00e096
diegofdg <diegofdg@gmail.com>
2023-11-26 09:28:58 -0300

    01_bienesraices_mvc-reescribiendo el objeto en la base de datos

--- 01_bienesraices_mvc/controllers/propiedadController.js
+++ 01_bienesraices_mvc/controllers/propiedadController.js
@@ -221,7 +221,30 @@ const guardarCambios = async (req, res ) => {
     }
 
     // Reescribir el objeto y actualizarlo    
+    try {
 
+        const { titulo, descripcion, habitaciones, estacionamiento, wc, calle, lat, lng, precio: precioId, categoria: categoriaId } = req.body;
+
+        propiedad.set({
+            titulo,
+            descripcion,
+            habitaciones,
+            estacionamiento,
+            wc,
+            calle,
+            lat,
+            lng,
+            precioId,
+            categoriaId
+        });
+        
+        await propiedad.save();
+
+        res.redirect('/mis-propiedades');
+        
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export {
