db4dda82bbfdbc24d3faafe0f5eab0f708a6e436
diegofdg <diegofdg@gmail.com>
2023-12-07 07:32:14 -0300

    01_bienesraices_mvc-mostrando los pines de las propiedades

--- 01_bienesraices_mvc/src/js/mapaInicio.js
+++ 01_bienesraices_mvc/src/js/mapaInicio.js
@@ -3,6 +3,8 @@
     const lng = -118.1618621;
     const mapa = L.map('mapa-inicio').setView([lat, lng ], 13);
 
+    let markers = new L.FeatureGroup().addTo(mapa)
+
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mapa);
@@ -12,10 +14,23 @@
             const url = '/api/propiedades';
             const respuesta = await fetch(url);
             const propiedades = await respuesta.json();
-            console.log(propiedades);
+            mostrarPropiedades(propiedades);
         } catch (error) {
             console.log(error);
         }
+    }
+
+    const mostrarPropiedades = propiedades => {
+
+        propiedades.forEach(propiedad => {
+            // Agregar los pines
+            const marker = new L.marker([propiedad?.lat, propiedad?.lng ], {
+                autoPan: true
+            })
+            .addTo(mapa)
+            .bindPopup('Información aqui');
+            markers.addLayer(marker)
+        })
     }
 
     obtenerPropiedades();
