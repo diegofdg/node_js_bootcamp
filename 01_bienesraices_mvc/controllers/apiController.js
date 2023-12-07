ea7a6a9f6d7a0a0c9bcaf57d57b462b8e48975ad
diegofdg <diegofdg@gmail.com>
2023-12-07 07:19:35 -0300

    01_bienesraices_mvc-finalizando el json de propiedades

--- 01_bienesraices_mvc/controllers/apiController.js
+++ 01_bienesraices_mvc/controllers/apiController.js
@@ -1,5 +1,14 @@
-const propiedades = async (req, res) => {    
-    res.json({respuesta: 'Ok'});
+import { Propiedad, Precio, Categoria } from '../models/index.js';
+
+const propiedades = async (req, res) => {
+
+    const propiedades = await Propiedad.findAll({
+        include: [
+            {model: Precio, as: 'precio'},
+            {model: Categoria, as: 'categoria'},
+        ]
+    });
+    res.json(propiedades);
 }
 
 export {
