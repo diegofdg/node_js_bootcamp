0dc0447cfc913ea9d2607224156fcc23c2f428f5
diegofdg <diegofdg@gmail.com>
2023-12-12 07:06:10 -0300

    01_bienesraices_mvc-primeros pasos creando un filtrador de propiedades

--- 01_bienesraices_mvc/controllers/appController.js
+++ 01_bienesraices_mvc/controllers/appController.js
@@ -1,7 +1,19 @@
+import { Sequelize } from 'sequelize';
+import { Precio, Categoria, Propiedad } from '../models/index.js';
+
 const inicio = async (req, res) => {
+    const [ categorias, precios ] = await Promise.all([
+        Categoria.findAll({raw: true}),
+        Precio.findAll({raw: true})        
+    ]);
+
+    console.log(categorias);
+
     res.render('inicio', {
-        pagina: 'Inicio'
-    })
+        pagina: 'Inicio',
+        categorias,
+        precios
+    });
 }    
 
 const categoria = async (req, res) => {
