84e5ad8795d692e7ceea1ca531b8ab2aa96b2d52
diegofdg <diegofdg@gmail.com>
2023-12-14 06:43:14 -0300

    01_bienesraices_mvc-obteniendo casas y departamentos para la página principal

--- 01_bienesraices_mvc/controllers/appController.js
+++ 01_bienesraices_mvc/controllers/appController.js
@@ -2,9 +2,39 @@ import { Sequelize } from 'sequelize';
 import { Precio, Categoria, Propiedad } from '../models/index.js';
 
 const inicio = async (req, res) => {
-    const [ categorias, precios ] = await Promise.all([
+    const [ categorias, precios, casas, departamentos ] = await Promise.all([
         Categoria.findAll({raw: true}),
-        Precio.findAll({raw: true})        
+        Precio.findAll({raw: true}),
+        Propiedad.findAll({
+            limit: 3,
+            where: { 
+                categoriaId: 1
+            },
+            include: [
+                {
+                    model: Precio, 
+                    as: 'precio'
+                }
+            ], 
+            order: [
+                ['createdAt', 'DESC']
+            ]
+        }),
+        Propiedad.findAll({
+            limit: 3,
+            where: { 
+                categoriaId: 2
+            },
+            include: [
+                {
+                    model: Precio, 
+                    as: 'precio'
+                }
+            ], 
+            order: [
+                ['createdAt', 'DESC']
+            ]
+        })
     ]);
 
     console.log(categorias);
@@ -12,7 +42,9 @@ const inicio = async (req, res) => {
     res.render('inicio', {
         pagina: 'Inicio',
         categorias,
-        precios
+        precios,
+        casas,
+        departamentos
     });
 }    
 
