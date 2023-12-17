39e7c592c8259e1d4abd3ae7bf00867cd106ddb0
diegofdg <diegofdg@gmail.com>
2023-12-16 21:30:57 -0300

    01_bienesraices_mvc-creando las páginas de categorías

--- 01_bienesraices_mvc/controllers/appController.js
+++ 01_bienesraices_mvc/controllers/appController.js
@@ -49,7 +49,28 @@ const inicio = async (req, res) => {
 }    
 
 const categoria = async (req, res) => {
-    
+    const { id } = req.params;
+
+    // Comprobar que la categoria exista
+    const categoria = await Categoria.findByPk(id)
+    if(!categoria) {
+        return res.redirect('/404');
+    }
+
+    // Obtener las propiedades de la categoria
+    const propiedades = await Propiedad.findAll({
+        where: {
+            categoriaId: id
+        }, 
+        include: [
+            { model: Precio, as: 'precio'}
+        ]
+    });
+
+    res.render('categoria', {
+        pagina: `${categoria.nombre}s en Venta`,
+        propiedades
+    });    
 }
 
 const noEncontrado = (req, res) => {
