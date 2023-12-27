9fbf1208ecdbbf4a05b50ad185cdcc91e9cde2d0
diegofdg <diegofdg@gmail.com>
2023-12-27 07:08:15 -0300

    02_devjobs-definiendo el modelo de vacantes

--- 02_devjobs/config/db.js
+++ 02_devjobs/config/db.js
@@ -5,3 +5,6 @@ mongoose.connect( process.env.DB_URL, { useNewUrlParser: true });
 mongoose.connection.on('error', (error) => {
     console.log(error);
 });
+
+// importar los modelos
+require('../models/Vacantes');
