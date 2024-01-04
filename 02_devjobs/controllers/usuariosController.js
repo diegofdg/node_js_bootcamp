01862dd72a9620aeddfd1efbf2a6767b5ddaaab7
diegofdg <diegofdg@gmail.com>
2024-01-04 19:54:45 -0300

    02_devjobs-almacenando los usuarios en la base de datos

--- 02_devjobs/controllers/usuariosController.js
+++ 02_devjobs/controllers/usuariosController.js
@@ -1,6 +1,21 @@
+const mongoose = require('mongoose');
+const Usuarios = mongoose.model('Usuarios');
+
 exports.formCrearCuenta = (req, res) => {
     res.render('crear-cuenta', {
         nombrePagina: 'Crea tu cuenta en devJobs',
         tagline: 'Comienza a publicar tus vacantes gratis, solo debes crear una cuenta'
     })
+}
+
+exports.crearUsuario = async (req, res, next) => {
+    // crear el usuario
+    const usuario = new Usuarios(req.body);
+    try {
+        await usuario.save();
+        res.redirect('/iniciar-sesion');
+    } catch (error) {
+        req.flash('error', error);
+        res.redirect('/crear-cuenta');
+    }
 }
