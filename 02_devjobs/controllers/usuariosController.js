ad6b85822682505f5fbef7bc26ac867be059bd47
diegofdg <diegofdg@gmail.com>
2024-01-06 19:58:40 -0300

    02_devjobs-guardando los cambios en el perfil en la base de datos

--- 02_devjobs/controllers/usuariosController.js
+++ 02_devjobs/controllers/usuariosController.js
@@ -62,9 +62,25 @@ exports.formIniciarSesion = (req, res ) => {
 
 // Form editar el Perfil
 exports.formEditarPerfil = (req, res) => {
-    console.log(req.user)
     res.render('editar-perfil', {
         nombrePagina : 'Edita tu perfil en devJobs',
         usuario: req.user,
     });
+}
+
+// Guardar cambios editar perfil
+exports.editarPerfil = async (req, res) => {
+    const usuario = await Usuarios.findById(req.user._id);
+
+    usuario.nombre = req.body.nombre;
+    usuario.email = req.body.email;
+    if(req.body.password) {
+        usuario.password = req.body.password
+    }
+
+    await usuario.save();
+
+    req.flash('correcto', 'Cambios Guardados Correctamente');
+    // redirect
+    res.redirect('/administracion');
 }
