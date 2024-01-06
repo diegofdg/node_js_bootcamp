3f7a3021d0bbfda4a7f330f65eb94a6651476c86
diegofdg <diegofdg@gmail.com>
2024-01-06 05:43:54 -0300

    02_devjobs-crear un formulario de autenticación

--- 02_devjobs/routes/index.js
+++ 02_devjobs/routes/index.js
@@ -32,6 +32,9 @@ module.exports = () => {
         usuariosController.validarRegistro,
         usuariosController.crearUsuario
     );
+
+    // Autenticar Usuarios
+    router.get('/iniciar-sesion', usuariosController.formIniciarSesion);
     
     return router;
 }
