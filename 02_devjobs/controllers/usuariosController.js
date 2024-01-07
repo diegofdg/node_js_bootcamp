0b3302c738e33b36d8a1bed1fba513799e45dcff
diegofdg <diegofdg@gmail.com>
2024-01-06 21:08:46 -0300

    02_devjobs-sanitizando y validando el perfil

--- 02_devjobs/controllers/usuariosController.js
+++ 02_devjobs/controllers/usuariosController.js
@@ -86,3 +86,33 @@ exports.editarPerfil = async (req, res) => {
     // redirect
     res.redirect('/administracion');
 }
+
+// sanitizar y validar el formulario de editar perfiles
+exports.validarPerfil = (req, res, next) => {
+    // sanitizar
+    req.sanitizeBody('nombre').escape();
+    req.sanitizeBody('email').escape();
+    if(req.body.password){
+        req.sanitizeBody('password').escape();
+    }
+    // validar
+    req.checkBody('nombre', 'El nombre no puede ir vacio').notEmpty();
+    req.checkBody('email', 'El correo no puede ir vacio').notEmpty();
+
+    const errores = req.validationErrors();
+
+    if(errores) {
+        req.flash('error', errores.map(error => error.msg));
+
+        res.render('editar-perfil', {
+            nombrePagina : 'Edita tu perfil en devJobs',
+            usuario: req.user,
+            cerrarSesion: true,
+            nombre : req.user.nombre,
+            imagen : req.user.imagen,
+            mensajes : req.flash()
+        });
+        return;
+    }
+    next(); // todo bien, siguiente middleware!
+}
--- 02_devjobs/routes/index.js
+++ 02_devjobs/routes/index.js
@@ -63,6 +63,7 @@ module.exports = () => {
     );
     router.post('/editar-perfil', 
         authController.verificarUsuario,
+        usuariosController.validarPerfil,
         usuariosController.editarPerfil
     );
     
