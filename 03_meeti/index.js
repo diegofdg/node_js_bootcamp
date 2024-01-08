1841931e4a6177bffb9ced604ed48b2b614bd958
diegofdg <diegofdg@gmail.com>
2024-01-08 11:40:26 -0300

    03_meeti-agregando el routing

--- 03_meeti/index.js
+++ 03_meeti/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const router = require('./routes');
 
 require('dotenv').config();
 
 const app = express();
+
+// Routing
+app.use('/', router());
 
 app.listen(process.env.PORT, () => {
     console.log('El servidor está funcionando')
