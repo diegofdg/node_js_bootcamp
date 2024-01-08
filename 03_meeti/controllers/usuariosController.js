b36ec7bef4ec21ed56516a755961b2f468604f24
diegofdg <diegofdg@gmail.com>
2024-01-08 20:05:58 -0300

    03_meeti-insertando usuarios en la base de datos

--- 03_meeti/controllers/usuariosController.js
+++ 03_meeti/controllers/usuariosController.js
@@ -1,5 +1,18 @@
+const Usuarios = require('../models/Usuarios');
+
 exports.formCrearCuenta = async (req, res) => {
     res.render('crear-cuenta', {
         nombrePagina : 'Crea tu Cuenta'
     });
 };
+
+exports.crearNuevaCuenta = async (req, res) => {
+    const usuario = req.body;
+    
+    const nuevoUsuario = await Usuarios.create(usuario);
+
+    console.log(nuevoUsuario);
+
+    
+    
+}
--- 03_meeti/index.js
+++ 03_meeti/index.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const path = require('path');
+const bodyParser = require('body-parser');
 const router = require('./routes');
 
+// Configuración y Modelos BD
 const db = require('./config/db');
 require('./models/Usuarios');
 db.sync().then(() => console.log('DB Conectada')).catch((error) => console.log(error));
 
+// Variables de Desarrollo
 require('dotenv').config();
 
+// Aplicación Principal
 const app = express();
+
+// Body parser, leer formularios
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true }));
 
 // Habilitar EJS como template engine
 app.use(expressLayouts);
