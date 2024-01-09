b710088cf110d76d94792d58bbb2e096d4d9739b
diegofdg <diegofdg@gmail.com>
2024-01-09 08:00:24 -0300

    03_meeti-creando el template para el email

--- 03_meeti/handlers/emails.js
+++ 03_meeti/handlers/emails.js
@@ -17,12 +17,15 @@ exports.enviarEmail = async (opciones) => {
     console.log(opciones);
 
     // leer el archivo para el mail
+    const archivo = __dirname + `/../views/emails/${opciones.archivo}.ejs`;
 
     // compilarlo
+    const compilado = ejs.compile(fs.readFileSync(archivo, 'utf8'));
 
     // crear el HTML
+    const html = compilado({ url : opciones.url });
 
     // configurar las opciones del email
-
+    
     // enviar el mail
 }
