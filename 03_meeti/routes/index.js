e2efabeb00e572426b1d9b0c794a7abdabf20c40
diegofdg <diegofdg@gmail.com>
2024-01-09 11:49:59 -0300

    03_meeti-creando el template para el panel

--- 03_meeti/routes/index.js
+++ 03_meeti/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const homeController = require('../controllers/homeController');
 const usuariosController = require('../controllers/usuariosController');
 const authController = require('../controllers/authController');
+const adminController = require('../controllers/adminController');
 
 module.exports = function() {
 
@@ -16,6 +17,9 @@ module.exports = function() {
     // Iniciar sesion
     router.get('/iniciar-sesion', usuariosController.formIniciarSesion);
     router.post('/iniciar-sesion', authController.autenticarUsuario);
+
+    /** Panel de administración */
+    router.get('/administracion', adminController.panelAdministracion);
 
     return router;
 }
