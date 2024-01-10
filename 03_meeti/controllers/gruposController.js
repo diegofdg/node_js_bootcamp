5ba246e08ecf99fd72c06b076bb37d2086d314b4
diegofdg <diegofdg@gmail.com>
2024-01-10 07:05:39 -0300

    03_meeti-sanitizando grupos

--- 03_meeti/controllers/gruposController.js
+++ 03_meeti/controllers/gruposController.js
@@ -12,7 +12,14 @@ exports.formNuevoGrupo = async (req, res) => {
 
 // Almacena los grupos en la BD
 exports.crearGrupo = async (req, res) => {
+    // sanitizar
+    req.sanitizeBody('nombre');
+    req.sanitizeBody('url');
+
     const grupo = req.body;
+
+    // almacena el usuario autenticado como el creador del grupo
+    grupo.usuarioId = req.user.id;
 
     try {
         // almacenar en la bD
@@ -20,7 +27,9 @@ exports.crearGrupo = async (req, res) => {
         req.flash('exito', 'Se ha creado el Grupo Correctamente');
         res.redirect('/administracion');
     } catch (error) {
-        req.flash('error', error);
+        // extraer el message de los errores
+        const erroresSequelize = error.errors.map(err => err.message);
+        req.flash('error', erroresSequelize);
         res.redirect('/nuevo-grupo');
     }
 }
