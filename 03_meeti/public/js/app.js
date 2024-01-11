1466b4bc87ac957c10e8e0de031d9bf458344a6b
diegofdg <diegofdg@gmail.com>
2024-01-11 09:09:09 -0300

    03_meeti-posicionando el pin en el resultado de la busqueda

--- 03_meeti/public/js/app.js
+++ 03_meeti/public/js/app.js
@@ -5,6 +5,8 @@ const lng = -103.3921777456999;
 
 const map = L.map('mapa').setView([lat, lng], 15);
 
+let marker;
+
 document.addEventListener('DOMContentLoaded', () => {
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -20,8 +22,17 @@ function buscarDireccion(e) {
         // Utilizar el provider
         const provider = new OpenStreetMapProvider();
         provider.search({ query: e.target.value }).then(( resultado ) => {
+            // mostrar el mapa
+            map.setView(resultado[0].bounds[0], 15);
+
             // agregar el pin
-            console.log(resultado);
+            marker = new L.marker(resultado[0].bounds[0], {
+                draggable : true,
+                autoPan: true
+            })
+            .addTo(map)
+            .bindPopup(resultado[0].label)
+            .openPopup();
         });
     }
 }
