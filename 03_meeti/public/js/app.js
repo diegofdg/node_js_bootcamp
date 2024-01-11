2b29c90d8f68249eed2df374e469254a549134ed
diegofdg <diegofdg@gmail.com>
2024-01-11 09:01:55 -0300

    03_meeti-configurando el mapa y geocoding

--- 03_meeti/public/js/app.js
+++ 03_meeti/public/js/app.js
@@ -1,6 +1,27 @@
-var map = L.map('mapa').setView([51.505, -0.09], 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-var marker = L.marker([51.5, -0.09]).addTo(map);
+import { OpenStreetMapProvider } from 'leaflet-geosearch';
+
+const lat = 20.666332695977;
+const lng = -103.3921777456999;
+
+const map = L.map('mapa').setView([lat, lng], 15);
+
+document.addEventListener('DOMContentLoaded', () => {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    // buscar la dirección
+    const buscador = document.querySelector('#formbuscador');
+    buscador.addEventListener('input', buscarDireccion);
+});
+
+function buscarDireccion(e) {
+    if(e.target.value.length > 8) {
+        // Utilizar el provider
+        const provider = new OpenStreetMapProvider();
+        provider.search({ query: e.target.value }).then(( resultado ) => {
+            // agregar el pin
+            console.log(resultado);
+        });
+    }
+}
