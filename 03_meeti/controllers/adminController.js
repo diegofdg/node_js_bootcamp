debb435adf46194e76de016e34a6bf3d2128b17f
diegofdg <diegofdg@gmail.com>
2024-01-11 16:10:20 -0300

    03_meeti-consultando los meetis

--- 03_meeti/controllers/adminController.js
+++ 03_meeti/controllers/adminController.js
@@ -1,10 +1,18 @@
 const Grupos = require('../models/Grupos');
+const Meeti = require('../models/Meeti');
 
 exports.panelAdministracion = async (req, res) => {
-    const grupos = await Grupos.findAll({ where: { usuarioId : req.user.id }});
-
+    // consultas
+    const consultas = [];
+    consultas.push( Grupos.findAll({ where: { usuarioId : req.user.id }}) );
+    consultas.push( Meeti.findAll({ where : { usuarioId : req.user.id }}) );
+    
+    // array destructuring
+    const [grupos, meeti] = await Promise.all(consultas);
+    
     res.render('administracion', {
-        nombrePagina : 'Panel de Administracion',
-        grupos
+        nombrePagina : 'Panel de Administracion', 
+        grupos, 
+        meeti
     });
 }
