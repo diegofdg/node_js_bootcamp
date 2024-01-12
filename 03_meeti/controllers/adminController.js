983949003b8209d695679aacc87e371947501cdd
diegofdg <diegofdg@gmail.com>
2024-01-12 07:00:54 -0300

    03_meeti-agregando meetis por pasados y próximos

--- 03_meeti/controllers/adminController.js
+++ 03_meeti/controllers/adminController.js
@@ -1,12 +1,19 @@
 const Grupos = require('../models/Grupos');
 const Meeti = require('../models/Meeti');
 const moment = require('moment');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 exports.panelAdministracion = async (req, res) => {
     // consultas
     const consultas = [];
     consultas.push( Grupos.findAll({ where: { usuarioId : req.user.id }}) );
-    consultas.push( Meeti.findAll({ where : { usuarioId : req.user.id }}) );
+    consultas.push( Meeti.findAll({ where : { usuarioId : req.user.id, 
+                fecha : { 
+                    [Op.gte] : moment(new Date()).format("YYYY-MM-DD") 
+                }
+            }
+    }));
     
     // array destructuring
     const [grupos, meeti] = await Promise.all(consultas);
