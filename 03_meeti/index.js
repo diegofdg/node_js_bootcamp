504e64381470c4fec278c4df516d3a193dc5261e
diegofdg <diegofdg@gmail.com>
2024-01-12 12:05:50 -0300

    03_meeti-mostrando una barra de navegación si el usuario está autenticado

--- 03_meeti/index.js
+++ 03_meeti/index.js
@@ -60,6 +60,7 @@ app.use(flash());
 
 // Middleware (usuario logueado, flash messages, fecha actual)
 app.use((req, res, next) => {
+    res.locals.usuario = {...req.user} || null;
     res.locals.mensajes = req.flash();
     const fecha = new Date();
     res.locals.year = fecha.getFullYear();
