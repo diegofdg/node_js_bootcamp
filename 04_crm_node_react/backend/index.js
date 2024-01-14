465d5995fdaae486b0ada1a5f327fad71d270439
diegofdg <diegofdg@gmail.com>
2024-01-14 11:36:18 -0300

    04_crm_node_react-conectando nuestro proyecto a mongodb

--- 04_crm_node_react/backend/index.js
+++ 04_crm_node_react/backend/index.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const routes = require('./routes');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// conectar mongo
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 // crear el servidor
 const app = express();
