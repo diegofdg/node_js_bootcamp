ae34c2b7ef59670946772352dd89fb254a4ca3ad
diegofdg <diegofdg@gmail.com>
2024-01-14 20:17:37 -0300

    04_crm_node_react-habilitar cors y consultando la api

--- 04_crm_node_react/backend/index.js
+++ 04_crm_node_react/backend/index.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
+// Cors permite que un cliente se conecta a otro servidor para el intercambio de recursos
+const cors = require('cors');
+
 // conectar mongo
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DB_URL, {
@@ -18,6 +21,9 @@ const app = express();
 // habilitar bodyparser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+// Habilitar cors
+app.use(cors());
 
 // Rutas de la app
 app.use('/', routes());
--- 04_crm_node_react/frontend/src/components/clientes/Clientes.js
+++ 04_crm_node_react/frontend/src/components/clientes/Clientes.js
@@ -1,6 +1,20 @@
-import React, { Fragment } from 'react';
+import React, { useEffect, Fragment } from 'react';
+
+// importar cliente axios
+import clienteAxios from '../../config/axios';
 
 function Clientes() {
+    // query a la API
+    const consultarAPI = async () => {
+        const clienteConsulta = await clienteAxios.get('/clientes');
+        console.log(clienteConsulta);
+    }
+
+    // use effect es similar a componentdidmount y willmount
+    useEffect( () => {
+        consultarAPI();        
+    }, [] );
+
     return (
         <Fragment>        
             <h2>Clientes</h2>
