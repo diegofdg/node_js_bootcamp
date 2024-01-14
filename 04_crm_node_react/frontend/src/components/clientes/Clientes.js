fa141c37407845356b4634378e539356ddb017f2
diegofdg <diegofdg@gmail.com>
2024-01-14 20:26:47 -0300

    04_crm_node_react-almacenando los resultados de la consulta en el state y usestate

--- 04_crm_node_react/frontend/src/components/clientes/Clientes.js
+++ 04_crm_node_react/frontend/src/components/clientes/Clientes.js
@@ -1,13 +1,18 @@
-import React, { useEffect, Fragment } from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
 
 // importar cliente axios
 import clienteAxios from '../../config/axios';
 
 function Clientes() {
+    // Trabajar con el state
+    // clientes = state,  guardarClientes = funcion para guardar el state
+    const [ clientes, guardarClientes ] = useState([]);
+
     // query a la API
     const consultarAPI = async () => {
-        const clienteConsulta = await clienteAxios.get('/clientes');
-        console.log(clienteConsulta);
+        const clientesConsulta = await clienteAxios.get('/clientes');
+        // colocar el resultado en el state
+        guardarClientes(clientesConsulta.data);
     }
 
     // use effect es similar a componentdidmount y willmount
@@ -18,6 +23,11 @@ function Clientes() {
     return (
         <Fragment>        
             <h2>Clientes</h2>
+            <ul className="listado-clientes">
+                {clientes.map(cliente => (
+                    console.log(cliente)
+                ))}
+            </ul>
         </Fragment>
     )
 }
