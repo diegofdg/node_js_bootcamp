2a1d3598ca5973d1bedd058611c86c735071c6bc
diegofdg <diegofdg@gmail.com>
2024-01-14 10:44:04 -0300

    03_meeti-finalizando las consultas para el buscador

--- 03_meeti/controllers/frontend/busquedaControllerFE.js
+++ 03_meeti/controllers/frontend/busquedaControllerFE.js
@@ -12,10 +12,40 @@ exports.resultadosBusqueda = async (req, res) => {
     
     const { categoria, titulo, ciudad, pais } = req.query;
 
+    // si la categoria esta vacia
+    let query;
+    if(categoria === ''){
+        query = '';
+    } else {
+        query = `where : {
+            categoriaId : { [Op.eq] :  ${categoria}  },
+        }`
+    }
+ 
     // filtrar los meetis por los terminos de busqueda
     const meetis = await Meeti.findAll({ 
         where :  { 
-
-        }
+            titulo : { [Op.iLike] :  '%' + titulo + '%' },
+            ciudad : { [Op.iLike] :  '%' + ciudad + '%' },
+            pais : { [Op.iLike] :  '%' + pais + '%' }
+        },
+        include: [
+            {
+                model: Grupos, 
+                query
+            },
+            { 
+                model: Usuarios, 
+                attributes : ['id',  'nombre', 'imagen']
+            }
+        ]
     });
+
+    // pasar los resultados a la vista
+    res.render('busqueda', {
+        nombrePagina : 'Resultados Búsqueda',
+        meetis, 
+        moment
+    })
+
 }
