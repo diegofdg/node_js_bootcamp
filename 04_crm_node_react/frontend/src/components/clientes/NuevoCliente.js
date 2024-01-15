6a99b42a7bb27a8ae1cab9311d9c39b45afaf083
diegofdg <diegofdg@gmail.com>
2024-01-15 11:33:43 -0300

    04_crm_node_react-validar formularios en react

--- 04_crm_node_react/frontend/src/components/clientes/NuevoCliente.js
+++ 04_crm_node_react/frontend/src/components/clientes/NuevoCliente.js
@@ -20,6 +20,18 @@ function NuevoCliente(){
         });
     }
 
+    // Validar el formulario
+    const validarCliente = () => {
+        // Destructuring
+        const { nombre, apellido, email, empresa, telefono } = cliente;
+
+        // revisar que las propiedades del state tengan contenido
+        let valido = !nombre.length || !apellido.length || !email.length || !empresa.length || !telefono.length;
+
+        // return true o false
+        return valido;
+    }
+
     return (
         <Fragment>
             <h2>Nuevo Cliente</h2>
@@ -80,6 +92,7 @@ function NuevoCliente(){
                         type="submit" 
                         className="btn btn-azul" 
                         value="Agregar Cliente"
+                        disabled={ validarCliente() }
                     />
                 </div>
             </form>
