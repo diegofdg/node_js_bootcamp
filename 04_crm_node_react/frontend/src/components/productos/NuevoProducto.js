8439b0479699bc8c242edeb6d32410047bf18f64
diegofdg <diegofdg@gmail.com>
2024-01-15 19:26:26 -0300

    04_crm_node_react-creando el formulario para nuevos productos

--- 04_crm_node_react/frontend/src/components/productos/NuevoProducto.js
+++ 04_crm_node_react/frontend/src/components/productos/NuevoProducto.js
@@ -1,9 +1,54 @@
-import React, { Fragment } from 'react';
+import React, { useState, Fragment } from 'react';
 
-function NuevoProducto() {
+function NuevoProducto(props) {
+
+    //producto = state, guardarProducto = setstate
+    const [producto, guardarProducto] = useState({
+        nombre: '',
+        precio: ''
+    });
+    // archivo = state, guardarArchivo = setState
+    const [archivo, guardarArchivo] = useState('');
+
     return (
-        <Fragment>        
-            <h2>NuevoProducto</h2>
+        <Fragment>
+            <h2>Nuevo Producto</h2>
+
+            <form>
+                <legend>Llena todos los campos</legend>
+
+                <div className="campo">
+                    <label>Nombre:</label>
+                    <input 
+                        type="text" 
+                        placeholder="Nombre Producto" 
+                        name="nombre"
+                    />
+                </div>
+
+                <div className="campo">
+                    <label>Precio:</label>
+                    <input 
+                        type="number" 
+                        name="precio" 
+                        min="0.00" 
+                        step="0.01" 
+                        placeholder="Precio"
+                    />
+                </div>
+
+                <div className="campo">
+                    <label>Imagen:</label>
+                    <input 
+                        type="file"  
+                        name="imagen"
+                    />
+                </div>
+
+                <div className="enviar">
+                        <input type="submit" className="btn btn-azul" value="Agregar Producto" />
+                </div>
+            </form>
         </Fragment>
     )
 }
