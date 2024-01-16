97b0133b1435553b2e61080c188f42c2b8265492
diegofdg <diegofdg@gmail.com>
2024-01-16 17:35:22 -0300

    04_crm_node_react-endpoint y controller para nuevos usuarios

--- 04_crm_node_react/backend/controllers/usuariosController.js
+++ 04_crm_node_react/backend/controllers/usuariosController.js
@@ -1,7 +1,18 @@
 const Usuarios = require('../models/Usuarios');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
-exports.registrarUsuario = async (req, res) => {    
-    
+exports.registrarUsuario = async (req, res) => {  
+    // leer los datos del usuario y colocarlos en Usuarios
+    const usuario = new Usuarios(req.body);
+    usuario.password = await bcrypt.hash(req.body.password, 12);
+    try {
+        await usuario.save();
+        res.json({mensaje : 'Usuario Creado Correctamente'});
+    } catch (error) {
+        console.log(error);
+        res.json({mensaje : 'Hubo un error'});
+    }
 }
 
 exports.autenticarUsuario = async (req, res, next) => {
