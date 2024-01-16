7cb0de8c2c80ec10b5d706541a5bf44e02889ed8
diegofdg <diegofdg@gmail.com>
2024-01-16 17:42:39 -0300

    04_crm_node_react-autenticar usuario y generar jwt

--- 04_crm_node_react/backend/controllers/usuariosController.js
+++ 04_crm_node_react/backend/controllers/usuariosController.js
@@ -16,5 +16,34 @@ exports.registrarUsuario = async (req, res) => {
 }
 
 exports.autenticarUsuario = async (req, res, next) => {
-    
+    // buscar el usuario
+    const { email, password } = req.body;
+    const usuario = await Usuarios.findOne({ email });
+
+    if(!usuario) {
+        // Si el usuario no existe
+        await res.status(401).json({mensaje : 'Ese usuario no existe'});
+        next();
+    } else {
+        // El usuario existe, verificar si el password es correcto o incorrecto
+        if(!bcrypt.compareSync(password, usuario.password )) {
+            // si el password es incorrecto
+            await res.status(401).json({ mensaje : 'Password Incorrecto'});
+            next();
+        } else {
+            // password correcto, firmar el token
+            const token = jwt.sign({
+                email : usuario.email, 
+                nombre: usuario.nombre, 
+                id : usuario._id
+            }, 
+            'LLAVESECRETA', 
+            {
+                expiresIn : '1h'
+            }); 
+
+            // retornar el TOKEN
+            res.json({ token });
+        }
+    }    
 }
