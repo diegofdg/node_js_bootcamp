c5c6d87329b382c460e85b90f5d03f0fbdc09322
diegofdg <diegofdg@gmail.com>
2024-01-16 18:10:29 -0300

    04_crm_node_react-enviando la petición al servidor para iniciar sesión

--- 04_crm_node_react/frontend/src/components/auth/Login.js
+++ 04_crm_node_react/frontend/src/components/auth/Login.js
@@ -1,16 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Swal from 'sweetalert2';
+import { withRouter } from 'react-router-dom';
+import clienteAxios from '../../config/axios';
 
-function Login(){
+function Login(props){
+
+    // State con los datos del formulario
+    const [ credenciales, guardarCredenciales] = useState({});
+
+    // iniciar sesión en el servidor
+    const iniciarSesion = async e => {
+        e.preventDefault();
+
+        // autenticar al usuario
+
+        try {
+            const respuesta = await clienteAxios.post('/iniciar-sesion', credenciales);
+            // extraer el token y colocarlo en localstorage
+            const { token } = respuesta.data;
+            localStorage.setItem('token', token);
+
+            // alerta
+            Swal.fire(
+                'Login Correcto',
+                'Has iniciado Sesión',
+                'success'
+            )
+
+            // redireccionar
+            props.history.push('/');
+            
+        } catch (error) {
+            console.log(error);
+            Swal.fire({
+                type: 'error',
+                title: 'Hubo un error',
+                text: error.response.data.mensaje
+            });
+        }
+    }
     
+    // almacenar lo que el usuario escribe en el state
     const leerDatos = e => {
-        
+        guardarCredenciales({
+            ...credenciales,
+            [e.target.name] : e.target.value
+        });        
     }
 
     return(
         <div className="login">
             <h2>Iniciar Sesión</h2>
             <div className="contenedor-formulario">
-                <form>
+                <form
+                    onSubmit={iniciarSesion}
+                >
                     <div className="campo">
                         <label>Email</label>
                         <input 
@@ -44,4 +88,4 @@ function Login(){
     )
 }
 
-export default Login;
+export default withRouter(Login);
