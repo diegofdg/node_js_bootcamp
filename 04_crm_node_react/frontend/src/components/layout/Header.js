e65d0b7f4c75f1fee5e9229be87e2c599c111d56
diegofdg <diegofdg@gmail.com>
2024-01-16 18:53:20 -0300

    04_crm_node_react-cerrar sesión y ocultar partes de la interfaz si el usuario no está autenticado

--- 04_crm_node_react/frontend/src/components/layout/Header.js
+++ 04_crm_node_react/frontend/src/components/layout/Header.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
-const Header = () => {
+import { CRMContext } from '../../context/CRMContext';
+import { withRouter } from 'react-router-dom';
+
+const Header = (props) => {
+
+    const [auth, guardarAuth] = useContext(CRMContext);
+
+    const cerrarSesion = () => {
+        // auth.auth = false y el token se remueve
+        guardarAuth({
+            token: '',
+            auth: false
+        });
+
+        localStorage.setItem('token', '');
+
+        // redireccionar
+        props.history.push('/iniciar-sesion');
+    }
+
     return (
         <header className="barra">
             <div className="contenedor">
                 <div className="contenido-barra">
                     <h1>CRM - Administrador de Clientes</h1>
-                </div>
+                    { auth.auth ? (
+                        <button 
+                            type="button"
+                            className="btn btn-rojo"
+                            onClick={cerrarSesion}
+                        >
+                            <i className="far fa-times-circle"></i>
+                            Cerrar Sesión
+                        </button>
+                    ) : null }
+                </div>                
             </div>
         </header>
     )
 }
 
-export default Header;
+export default withRouter(Header);
--- 04_crm_node_react/frontend/src/components/layout/Navegacion.js
+++ 04_crm_node_react/frontend/src/components/layout/Navegacion.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
 import { Link } from 'react-router-dom';
+import { CRMContext } from '../../context/CRMContext';
 
 const Navegacion = () => {
+    // eslint-disable-next-line
+    const [auth, guardarAuth] = useContext(CRMContext);
+
+    if(!auth.auth) return null;
     return ( 
         <aside className="sidebar col-3">
             <h2>Administración</h2>
